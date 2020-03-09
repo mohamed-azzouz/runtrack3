12e9faa7ab356dd7793daebcb43c441a04245fc6
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-03-09 10:26:02 +0100

    Commit job01 et job02

---
+++ jour01/job01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job01</title>
+	<script>
+		window.alert("HELLO JAVASCRIPT");
+	</script>
+</head>
+<body>
+
+</body>
+</html>
---
+++ jour01/job02/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Job01</title>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+</body>
+</html>
