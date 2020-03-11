1c8884fa2b94e5bf27140f6a8051c3521e4df2cd
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-03-11 11:33:17 +0100

    commit job01

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -2,7 +2,7 @@
 <html id="null">
 <head>
 	<title>Job01</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 	<script type="text/javascript" src="script.js"></script>
 
 </head>
